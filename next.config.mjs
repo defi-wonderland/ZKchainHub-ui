5be359a6f8dd09cc290056a97a9bc132f681ba8a
titix <titi@defi.sucks>
2024-07-17 18:11:15 -0300

    fix: vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-    i18n: {
-        locales: ['en', 'es'],
-        defaultLocale: 'en',
-      },
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  localePath: path.resolve('./public/locales'),
+  outputFileTracing: true
 };
 
 export default nextConfig;
