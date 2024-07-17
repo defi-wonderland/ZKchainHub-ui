c43abdbc67a74b89c6f4ec81b976ae18e3114c41
titix <titi@defi.sucks>
2024-07-17 15:46:06 -0300

    feat: i18n set up

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-i18next').UserConfig} */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    i18n: {
+      locales: ['en', 'es'],
+      defaultLocale: 'en',
+    },
+  };
