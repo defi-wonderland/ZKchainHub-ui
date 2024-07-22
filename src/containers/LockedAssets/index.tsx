ae661226bf51296e4406d60b93ff7712f4bb4294
TITI <162849030+0xtiti@users.noreply.github.com>
2024-07-22 17:51:48 -0300

    feat: format number (#5)
    closes ZKS-111

--- src/containers/LockedAssets/index.tsx
+++ src/containers/LockedAssets/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'next-i18next';
 
 import { TotalValueLocked, Title } from '~/components';
 import { useData } from '~/hooks';
+import { formatDataNumber } from '~/utils';
 
 export const LockedAssets = () => {
   const { t } = useTranslation();
@@ -11,7 +12,7 @@ export const LockedAssets = () => {
     <section>
       {ecosystemData && (
         <>
-          <Title title={`${t('HOME.lockedAssets')}: ${ecosystemData.total}`} />
+          <Title title={`${t('HOME.lockedAssets')}: ${formatDataNumber(ecosystemData.total, 0, true, true)}`} />
           <TotalValueLocked tvl={ecosystemData.tvl} />
         </>
       )}
