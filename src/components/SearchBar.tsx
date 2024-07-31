a7769f1d3340bb255b034ac34a640bd5cf87f7a1
titix <titi@defi.sucks>
2024-07-31 13:21:44 -0300

    feat: header components styling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,54 @@
 import { useTranslation } from 'next-i18next';
+import { useCustomTheme, useStateContext } from '~/hooks';
+import { styled, TextField, InputAdornment } from '@mui/material';
+import Image from 'next/image';
 
-interface SearchBarProps {
-  value: string;
-  onChange: (value: string) => void;
-}
+import Search from '~/assets/icons/search.svg';
 
-export const SearchBar = ({ value, onChange }: SearchBarProps) => {
+export const SearchBar = () => {
   const { t } = useTranslation();
-
+  const { searchTerm, setSearchTerm } = useStateContext();
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value);
+    const value = event.target.value;
+    setSearchTerm(value);
   };
-
   return (
-    <form>
-      <input type='text' value={value} onChange={handleChange} placeholder={t('HOME.DASHBOARD.search')} />
-    </form>
+    <StyledTextField
+      variant='outlined'
+      value={searchTerm}
+      onChange={handleChange}
+      placeholder={t('HOME.DASHBOARD.search')}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position='start'>
+            <Image src={Search} alt='search' />
+          </InputAdornment>
+        ),
+      }}
+    />
   );
 };
+
+const StyledTextField = styled(TextField)(() => {
+  const { currentTheme } = useCustomTheme();
+
+  return {
+    width: '15rem',
+    height: '3.5rem',
+    borderRadius: `${currentTheme.borderRadius}`,
+    border: `1px solid ${currentTheme.backgroundSecondary}`,
+    backgroundColor: `${currentTheme.backgroundTertiary}`,
+    opacity: 1,
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        border: 'none',
+      },
+      '&:hover fieldset': {
+        border: 'none',
+      },
+      '&.Mui-focused fieldset': {
+        border: 'none',
+      },
+    },
+  };
+});
