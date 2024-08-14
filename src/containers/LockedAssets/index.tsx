4d2305938122c338502d4ac09deb49248e7c668e
titix <titi@defi.sucks>
2024-08-14 17:35:25 -0300

    feat: all tokens btn

--- src/containers/LockedAssets/index.tsx
+++ src/containers/LockedAssets/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
-import { Box, Typography, styled } from '@mui/material';
+import { useRouter } from 'next/router';
+import { Box, Typography, styled, Button } from '@mui/material';
 
 import { TotalValueLocked } from '~/components';
 import { useData, useCustomTheme } from '~/hooks';
@@ -8,6 +9,11 @@ import { formatDataNumber } from '~/utils';
 export const LockedAssets = () => {
   const { t } = useTranslation();
   const { ecosystemData, totalL1TVL } = useData();
+  const router = useRouter();
+
+  const goToTokensPage = () => {
+    router.push('/tokens');
+  };
 
   return (
     <StyledSection>
@@ -18,9 +24,14 @@ export const LockedAssets = () => {
               <Title>{t('HOME.LOCKEDASSETS.lockedAssets')}</Title>
               <Subtitle>{t('HOME.LOCKEDASSETS.lockedAssetsDescription')}</Subtitle>
             </Box>
+
             <TitleAmount>{formatDataNumber(totalL1TVL, 0, true, true)}</TitleAmount>
           </LockedAssetsContainer>
+
           <TotalValueLocked tvl={ecosystemData.l1Tvl} />
+          <ButtonContainer>
+            <AllTokensButton onClick={goToTokensPage}> {t('HOME.LOCKEDASSETS.allTokens')} </AllTokensButton>{' '}
+          </ButtonContainer>
         </>
       )}
     </StyledSection>
@@ -58,3 +69,27 @@ const TitleAmount = styled(Typography)(() => ({
   fontWeight: 700,
   lineHeight: '2.5rem',
 }));
+
+const AllTokensButton = styled(Button)(() => {
+  const { currentTheme } = useCustomTheme();
+  return {
+    color: currentTheme.textPrimary,
+    backgroundColor: currentTheme.backgroundSecondary,
+    borderRadius: currentTheme.borderRadius,
+    padding: '0.5rem 1rem',
+    gap: currentTheme.gap,
+    width: '5.5rem',
+    height: '2.25rem',
+    textTransform: 'none',
+    fontSize: '0.75rem',
+    marginTop: '1.5rem',
+    alignItems: 'center',
+  };
+});
+
+export const ButtonContainer = styled(Box)(() => {
+  return {
+    display: 'flex',
+    justifyContent: 'center',
+  };
+});
