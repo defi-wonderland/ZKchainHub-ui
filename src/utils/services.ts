e96663e3441233939af36aedbea240976b676434
titix <titi@defi.sucks>
2024-08-16 00:46:08 -0300

    fix: backend integration

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,19 +1,9 @@
 import { getConfig } from '~/config';
-import ecosystemMockData from '~/data/ecosystemMockData.json';
-import chainMockData from '~/data/chainMockData.json';
-
 const { API_URL } = getConfig();
-
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 console.log('Fetching backend data from:', API_URL);
 
 export const fetchEcosystemData = async () => {
-  // temporary for mock data
-  if (!API_URL) {
-    await delay(2000); // Simulate 2 seconds delay
-    return Promise.resolve(ecosystemMockData);
-  }
   const res = await fetch(`${API_URL}/metrics/ecosystem`);
   if (!res.ok) {
     throw new Error('Failed to fetch ecosystem data');
@@ -22,11 +12,6 @@ export const fetchEcosystemData = async () => {
 };
 
 export const fetchChainData = async (chainId: string) => {
-  // temporary for mock data
-  if (!API_URL) {
-    await delay(2000); // Simulate 2 seconds delay
-    return Promise.resolve(chainMockData);
-  }
   const res = await fetch(`${API_URL}/metrics/zkchain/${chainId}`);
   if (!res.ok) {
     throw new Error('Failed to fetch chain data');
