efecdfd889c1fd154880168550549f7ba0690037
titix <titi@defi.sucks>
2024-08-21 14:07:12 -0300

    feat: chain number format

--- src/components/RPC.tsx
+++ src/components/RPC.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useTranslation } from 'next-i18next';
-import { Box, Typography, Tooltip, styled } from '@mui/material';
+import { Box, Typography, Tooltip, styled, Skeleton } from '@mui/material';
 import { CheckCircle as CheckIcon, Cancel as CancelIcon } from '@mui/icons-material';
 
 import { useData, useCustomTheme } from '~/hooks';
@@ -11,9 +11,11 @@ export const RPC = () => {
   const { t } = useTranslation();
   const { chainData } = useData();
   const [rpcData, setRpcData] = useState<{ url: string; status: boolean }[]>([]);
+  const [rpcIsLoading, setRpcIsLoading] = useState(true);
 
   useEffect(() => {
     const updateRpcStatuses = async () => {
+      setRpcIsLoading(true);
       if (!chainData?.metadata?.publicRpcs) return;
 
       const updatedRpcData = await Promise.all(
@@ -24,6 +26,7 @@ export const RPC = () => {
       );
 
       setRpcData(updatedRpcData);
+      setRpcIsLoading(false);
     };
 
     updateRpcStatuses();
@@ -33,14 +36,21 @@ export const RPC = () => {
     <article>
       <STitle>{t('CHAIN.RPC.title')}</STitle>
       <DataContainer>
-        {rpcData.map((rpc, index) => (
-          <RPCBox key={index}>
-            <Tooltip title={rpc.status ? t('CHAIN.RPC.statusActive') : t('CHAIN.RPC.statusInactive')}>
-              {rpc.status ? <CheckIcon color='success' /> : <CancelIcon color='error' />}
-            </Tooltip>
-            <RPCUrl>{rpc.url}</RPCUrl>
-          </RPCBox>
-        ))}
+        {rpcIsLoading
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <RPCBox key={index}>
+                <Skeleton variant='circular' width={20} height={20} />
+                <Skeleton variant='text' width='100%' />
+              </RPCBox>
+            ))
+          : rpcData.map((rpc, index) => (
+              <RPCBox key={index}>
+                <Tooltip title={rpc.status ? t('CHAIN.RPC.statusActive') : t('CHAIN.RPC.statusInactive')}>
+                  {rpc.status ? <CheckIcon color='success' /> : <CancelIcon color='error' />}
+                </Tooltip>
+                <RPCUrl>{rpc.url}</RPCUrl>
+              </RPCBox>
+            ))}
       </DataContainer>
     </article>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './config';
 export * from './format';
 export * from './services';
 export * from './addNetwork';
+export * from './misc';
