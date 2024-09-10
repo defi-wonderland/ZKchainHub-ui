ed07d194a45f646d46f7f82556b5eb04bdcfcb1f
titix <titi@defi.sucks>
2024-09-10 12:28:16 -0300

    test: chain page navigation

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -14,10 +14,6 @@ describe('Navigation tests', () => {
         statusCode: 200,
         body: chainData,
       }).as('fetchChainData');
-      cy.intercept('GET', '**/_next/data/**/324.json*', {
-        statusCode: 200,
-        body: chainData,
-      }).as('fetchNextData');
     });
 
     cy.visit('/');
@@ -37,12 +33,12 @@ describe('Navigation tests', () => {
     cy.getByTestId('search-bar').find('input').type('324');
     cy.getByTestId('chain-row').should('be.visible').click();
 
-    // cy.getByTestId('chain-id').should('be.visible').and('contain', '324');
-    // cy.url().should('include', '/324');
+    cy.getByTestId('chain-id').should('be.visible').and('contain', '324');
+    cy.url().should('include', '/324');
 
-    // cy.getByTestId('home-breadcrumb').click();
-    // cy.url().should('eq', 'http://localhost:5173/');
-    // cy.getByTestId('search-bar').find('input').should('have.value', '');
+    cy.getByTestId('home-breadcrumb').click();
+    cy.url().should('eq', 'http://localhost:5173/');
+    cy.getByTestId('search-bar').find('input').should('have.value', '');
   });
 
   it('should navigate to tokens page, on all tokens button click', () => {
@@ -56,8 +52,8 @@ describe('Navigation tests', () => {
   it('should navigate to chain page by clicking on dashboard chain row', () => {
     cy.getByTestId('chains-dashboard').should('be.visible');
     cy.getByTestId('chain-row').first().click();
-    // cy.url().should('include', '/324');
+    cy.url().should('include', '/324');
 
-    // cy.getByTestId('chain-id').should('be.visible').and('contain', '324');
+    cy.getByTestId('chain-id').should('be.visible').and('contain', '324');
   });
 });
